0c9bea2a9f011421c4a8859191d85429d10659d3
alvalenda <banysan@msn.com>
2022-11-09 10:21:57 -0300

    hello world

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export const Header = ({}: Props) => {
+  return <div>Header</div>;
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+type Props = {};
+
+export const Home = ({}: Props) => {
+  return (
+    <div>
+      Home
+      <p className="text-3xl font-bold underline">'Hello World!'</p>
+    </div>
+  );
+};
