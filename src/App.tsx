aebe5b28cda2100e07a7bb073d22c1ae70e7ef91
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-11 17:11:03 -0300

    feat(theme): implement `ThemeProvider`

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import './App.css'
 import Button from './components/Button'
 import { Title } from './components/Title'
+import { themeObject } from './shared/theme.config'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Title />
-      <Button />
-    </div>
+    <ThemeProvider theme={themeObject}>
+      <div className="App">
+        <Title />
+        <Button />
+      </div>
+    </ThemeProvider>
   )
 }
 
