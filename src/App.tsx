77fcdfb7e3534f29afeb5113d7bb7c28be6c2451
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-11 16:54:00 -0300

    style(app): playground with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
 import { Title } from './components/Title'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Title />
+      <Button />
     </div>
   )
 }
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,16 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-type Props = {};
+type Props = {}
 
 const StyledTitle = styled.h1`
+  font-size: 3.2em;
+  line-height: 1.1;
   color: rgba(255, 255, 255, 0.8);
   background-color: rgba(0, 0, 0, 0.8);
-`;
+`
 
-export function Title(props: Props) {
+export function Title({}: Props) {
   return (
     <div>
       <StyledTitle>Hello World of Styled Components!</StyledTitle>
     </div>
-  );
+  )
 }
