f507d35dac1da6d06f3b298aab69531cb8a94774
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-11 17:00:08 -0300

    feat(theme): create `theme` object

---
+++ src/shared/theme.config.ts
@@ -0,0 +1,27 @@
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#ff0000',
+    background: '#fff',
+    textPrimary: '#000',
+    textSecondary: '#fff',
+    buttonPrimary: '#0070f3',
+    buttonSecondary: '#ff0000',
+    buttonTertiary: '#fff',
+    buttonPrimaryText: '#fff',
+    buttonSecondaryText: '#fff',
+    buttonTertiaryText: '#000',
+    buttonDisabled: '#ccc',
+    buttonDisabledText: '#fff',
+  },
+  fonts: {
+    primary: 'Roboto, sans-serif',
+    secondary: 'Roboto, sans-serif',
+  },
+
+  fontSizes: {
+    small: '12px',
+    medium: '16px',
+    large: '20px',
+  },
+}
