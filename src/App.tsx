2d33167966f28800673e5d1b1baba9f6de5bc243
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-12 13:13:14 -0300

    feat(theme): implement `TheneProviderReducer`

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,21 @@
-import { useContext } from 'react'
-import { ThemeProvider } from 'styled-components'
 import './App.css'
-import Button from './components/Button'
-import ThemeButton from './components/ThemeButton'
+import Button from './components/shared/Button'
+import ThemeProviderReducer from './components/shared/themes/ThemeProviderReducer'
+import ThemeButton from './components/shared/ThemeButton'
 import { Title } from './components/Title'
-import { TalespireContext } from './shared/contexts/talespire/TalespireContext'
-import { themeObject as theme } from './shared/theme/theme.config'
+import { TalespireProvider } from './contexts/talespire/TalespireContext'
 
 function App() {
-  const { darkMode } = useContext<{ darkMode: boolean }>(
-    TalespireContext as any
-  )
   return (
-    <ThemeProvider
-      theme={{
-        colors: {
-          primary: darkMode
-            ? theme.colors.primaryDark
-            : theme.colors.primaryLight,
-
-          secondary: darkMode
-            ? theme.colors.secondaryDark
-            : theme.colors.secondaryLight,
-
-          background: darkMode
-            ? theme.colors.backgroundDark
-            : theme.colors.backgroundLight,
-
-          backgroundVar: darkMode
-            ? theme.colors.backgroundVarDark
-            : theme.colors.backgroundVarLight,
-
-          buttonPrimary: darkMode
-            ? theme.colors.buttonPrimaryDark
-            : theme.colors.buttonPrimaryLight,
-
-          buttonSecondary: darkMode
-            ? theme.colors.buttonSecondaryDark
-            : theme.colors.buttonSecondaryLight,
-
-          interactive: darkMode
-            ? theme.colors.interactiveDark
-            : theme.colors.interactiveLight,
-
-          inlineError: darkMode
-            ? theme.colors.inlineErrorDark
-            : theme.colors.inlineErrorLight,
-
-          buttonText: theme.colors.buttonText,
-          buttonDisabled: theme.colors.buttonDisabled,
-          disabledText: theme.colors.disabledText,
-        },
-        fonts: {
-          ...theme.fonts,
-        },
-
-        fontSizes: {
-          ...theme.fontSizes,
-        },
-      }}
-    >
-      <div className="App">
-        <Title />
-        <Button>Faço Nada</Button>
-        <ThemeButton />
-      </div>
-    </ThemeProvider>
+    <TalespireProvider>
+      <ThemeProviderReducer>
+        <div className="App">
+          <Title />
+          <Button>Faço Nada</Button>
+          <ThemeButton />
+        </div>
+      </ThemeProviderReducer>
+    </TalespireProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { TalespireProvider } from './shared/contexts/talespire/TalespireContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <TalespireProvider>
-      <App />
-    </TalespireProvider>
+    <App />
   </React.StrictMode>
 )
