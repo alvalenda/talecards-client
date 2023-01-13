8e9668b4192d99e881c257c3fb6378463910ad1b
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-13 09:44:17 -0300

    feat(Login): create `Login` page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Title } from '@/components/Title'
+import { ThemeButton } from '@/components/shared/ThemeButton'
+import styled from 'styled-components'
+import { Button } from '@/components/shared/Button'
 
 type Props = {}
 
-export default function Login({}: Props) {
-  return <div>Login</div>
+const ScreamWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${({ theme }) => theme.colors.background};
+
+  transition: background-color 0.8s ease-in;
+`
+
+export function Login({}: Props) {
+  return (
+    <ScreamWrapper>
+      <Title />
+      <ThemeButton />
+      <h1>404</h1>
+      <Button>Faço Nada</Button>
+    </ScreamWrapper>
+  )
 }
