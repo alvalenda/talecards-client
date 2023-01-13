0cb1bbda13675be90926c14ace4d6b971223e19d
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-13 09:45:26 -0300

    feat(app): test login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import styled from 'styled-components'
 import './App.css'
-import Button from './components/shared/Button'
-import ThemeProviderReducer from './components/shared/themes/ThemeProviderReducer'
-import ThemeButton from './components/shared/ThemeButton'
-import { Title } from './components/Title'
+import { ThemeProviderReducer } from './components/shared/themes/ThemeProviderReducer'
 import { TalespireProvider } from './contexts/talespire/TalespireContext'
+import { Login } from './pages/Login'
 
+const StyledApp = styled.div`
+  display: flex;
+  margin: 0;
+  min-width: 320px;
+  min-height: 100vh;
+  padding: 0;
+  place-items: center;
+
+  #root {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+`
 function App() {
   return (
     <TalespireProvider>
       <ThemeProviderReducer>
-        <div className="App">
-          <Title />
-          <Button>Faço Nada</Button>
-          <ThemeButton />
-        </div>
+        <StyledApp className="App">
+          <Login />
+        </StyledApp>
       </ThemeProviderReducer>
     </TalespireProvider>
   )
