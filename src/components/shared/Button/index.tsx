134441215c4fbe19e1106b6cd6127c5259c58fbb
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-13 10:41:53 -0300

    feat(Button): add `primary` & `secondary` buttons

--- src/components/shared/Button/index.tsx
+++ src/components/shared/Button/index.tsx
@@ -1,36 +1,56 @@
+import { prependOnceListener } from 'process'
 import styled from 'styled-components'
 
 type Props = {
   children?: React.ReactNode
   handleClick?: () => void
+  style?: 'primary' | 'secondary'
 }
 
-const StyledButton = styled.button`
-  border-radius: 8px;
-  border: 2px solid transparent;
-  padding: 0.3em 0.6em;
-  font-size: ${(props) => props.theme.fontSizes.small};
-  font-weight: 500;
-  font-family: inherit;
-  background-color: ${(props) => props.theme.colors.buttonSecondary};
-  color: ${(props) => props.theme.colors.buttonText};
-  cursor: pointer;
-  transition: background-color 0.8s ease-in, border-color 0.4s ease-in-out;
+const returnStyle = (style: string) => {
+  return styled.button`
+    border-radius: 8px;
+    padding: 0.3em 0.6em;
+    font-size: ${(props) => props.theme.fontSizes.small};
+    font-family: inherit;
+    font-weight: 400;
 
-  &:hover {
-    border-color: ${(props) => props.theme.colors.interactive};
-  }
+    border: ${style === 'primary' ? '2px solid transparent' : '2px solid'};
 
-  &:focus,
-  &:focus-visible {
-    border-color: ${(props) => props.theme.colors.interactive};
-  }
-`
+    background-color: ${style === 'primary'
+      ? (props) => props.theme.colors.buttonPrimary
+      : (props) => props.theme.colors.buttonSecondary};
+
+    color: ${style === 'primary'
+      ? (props) => props.theme.colors.buttonSecondary
+      : (props) => props.theme.colors.buttonPrimary};
+
+    cursor: pointer;
+    transition: all 0.8s ease-in, border-color 0.4s ease-in, color 0.4s ease-in;
+
+    &:hover {
+      border-color: ${(props) => props.theme.colors.interactive};
+
+      color: ${(props) => props.theme.colors.interactive};
+    }
+
+    &:focus,
+    &:focus-visible {
+      border-color: ${(props) => props.theme.colors.interactive};
+
+      color: ${(props) => props.theme.colors.interactive};
+    }
+  `
+}
+
 export function Button({
   children = 'Button',
   handleClick = () => {
     console.log('Clicked')
   },
+  style = 'primary',
 }: Props) {
+  const StyledButton = returnStyle(style)
+
   return <StyledButton onClick={handleClick}>{children}</StyledButton>
 }
