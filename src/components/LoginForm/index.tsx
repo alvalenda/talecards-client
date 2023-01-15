9ff67cc8bc0a716835d0c8aa74356d0c8fed9bf4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-15 00:13:31 -0300

    feat(login): implement and style input

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { FaHatWizard } from 'react-icons/fa'
+import { MdLock } from 'react-icons/md'
 
 type Props = {
   children?: React.ReactNode
@@ -9,9 +11,9 @@ const StyledLoginSection = styled.section`
   align-items: center;
   align-content: center;
   vertical-align: middle;
-  border-radius: 8px;
+  border-radius: 12px;
   margin: 12em auto 2em auto;
-  padding: 0;
+  padding: 2em 0 0 0;
 
   height: calc(35rem);
   width: calc(90vw - 4em);
@@ -26,26 +28,60 @@ const StyledLoginSection = styled.section`
 const StyledLoginForm = styled.form`
   display: flex;
   position: relative;
+  margin: 0 auto;
   flex-direction: column;
-  align-items: flex-end;
+  align-items: center;
   justify-content: center;
 
-  .login-wrapper {
-    position: absolute;
-    width: 30rem;
-    top: 5rem;
-    left: calc(50% - 8rem);
+  .form-wrapper {
+    display: flex;
+    width: 21rem;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0.4rem 0;
+    gap: 0.5rem;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 15rem;
+    flex-direction: row;
+  }
+
+  svg {
+    vertical-align: middle;
+    margin-right: 0.2rem;
   }
 
   label {
     text-align: start;
+    vertical-align: middle;
   }
 
-  .password-wrapper {
-    position: absolute;
-    width: 30rem;
-    top: 7rem;
-    left: calc(50% - 10.3rem);
+  input {
+    width: 12rem;
+    height: 1.8rem;
+    padding: 0.5rem;
+    border-radius: 12px;
+    flex-basis: 1rem;
+    font-size: ${({ theme }) => theme.fontSizes.small};
+
+    border: 2px solid transparent;
+    outline: 1px solid ${({ theme }) => theme.colors.secondary};
+
+    background-color: ${({ theme }) => theme.colors.background};
+
+    color: ${({ theme }) => theme.colors.secondary};
+
+    transition: background-color 0.4s linear, border-color 0.3s ease;
+  }
+
+  input:focus {
+    outline: none;
+
+    border-color: ${({ theme }) => theme.colors.interactive};
   }
 `
 
@@ -53,12 +89,18 @@ export function LoginForm({ children }: Props) {
   return (
     <StyledLoginSection>
       <StyledLoginForm>
-        <div className="login-wrapper">
-          <label htmlFor="inputEmail">EMAIL</label>
+        <div className="form-wrapper">
+          <div>
+            <FaHatWizard size={24} />
+            <label htmlFor="inputEmail">EMAIL</label>
+          </div>
           <input type="email" name="email" id="email" />
         </div>
-        <div className="password-wrapper">
-          <label htmlFor="inputPassword">PASSWORD</label>
+        <div className="form-wrapper">
+          <div>
+            <MdLock size={24} />
+            <label htmlFor="inputPassword">PASSWORD</label>
+          </div>
           <input type="password" name="password" id="password" />
         </div>
       </StyledLoginForm>
@@ -67,6 +109,3 @@ export function LoginForm({ children }: Props) {
     </StyledLoginSection>
   )
 }
-
-// TODO - Add a button to show/hide password
-// Criar um container para email e password, e seus labels. Dividir em duas colunas e duas linhas. Alinhas labels no começo e inputs no fim. Colocar um espaço entre eles. Colocar um espaço entre o container e o botão.
