ec417127a26f8738f8c7390c541b428a938c32d3
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-15 14:36:01 -0300

    feat(LoginPage): Rename from `Login`

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,29 @@
+import { LoginForm } from '@/components/LoginForm'
+import { Button } from '@/components/shared/Button'
+import { Header } from '@/components/shared/Header'
+import { ScreenWrapper } from '@/components/shared/styles/StyledScreenWrapper'
+import styled from 'styled-components'
+
+type Props = {}
+
+export function LoginPage({}: Props) {
+  return (
+    <ScreenWrapper>
+      <Header />
+      <LoginForm>
+        <ButtonsWrapper>
+          <Button style="primary" text="Faço Nada" />
+          <Button style="secondary" text="Faço Tudo" />
+        </ButtonsWrapper>
+      </LoginForm>
+    </ScreenWrapper>
+  )
+}
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  place-items: center;
+  margin: 2rem auto 0 auto;
+  width: 16em;
+`
