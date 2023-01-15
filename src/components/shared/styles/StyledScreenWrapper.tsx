0b0294ad64645e0691d130ceb52aa76753ce95f8
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-15 20:54:48 -0300

    style(ScreenWrapper): ajusting css variables

--- src/components/shared/styles/StyledScreenWrapper.tsx
+++ src/components/shared/styles/StyledScreenWrapper.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components'
 
 export const ScreenWrapper = styled.section`
   display: flex;
-  border-radius: 16px;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   min-height: 100vh;
+  max-height: 200vh;
   width: 100vw;
+
   background-color: ${({ theme }) => theme.colors.background};
   color: ${({ theme }) => theme.colors.primary};
 
@@ -22,5 +21,6 @@ export const ScreenWrapper = styled.section`
     right: 0;
     bottom: 0;
     opacity: 0.8;
+    z-index: 0;
   }
 `
