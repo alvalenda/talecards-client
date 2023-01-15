6753f46b3c9fc4aa973102db07a633bcb29bea10
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-15 20:55:03 -0300

    feat(footer): implement `footer`

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,22 @@
 import { FooterWrapper } from './FooterWrapper'
 import { StyledFooter } from './StyledFooter'
+import logo from '@/assets/logo-av.png'
 
 type Props = {}
 
 export function Footer({}: Props) {
+  const currentYear = new Date().getFullYear()
   return (
     <FooterWrapper>
-      <StyledFooter></StyledFooter>
+      <StyledFooter>
+        <div>
+          <img src={logo} alt="alvarenga logo" />
+        </div>
+        <div>
+          <p>All rights reserved </p>
+          <p>© {currentYear}</p>
+        </div>
+      </StyledFooter>
     </FooterWrapper>
   )
 }
