cdb7b959c7076a951f599abc5b702a9ab31a7c69
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-16 20:10:53 -0300

    feat(aboutPage): implement `AboutCard`

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,17 +1,27 @@
+import { AboutCard } from '@/components/AboutCard'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { StyledCard } from '@/components/styles/StyledCard'
+import { Button } from '@/components/shared/Button'
 import { ScreenWrapper } from '@/components/styles/StyledScreenWrapper'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {}
 
 export function AboutPage({}: Props) {
+  const navigate = useNavigate()
+
+  const navToHome = () => {
+    navigate('/home')
+  }
+
   return (
     <ScreenWrapper>
       <Header />
-      <StyledCard>
-        <h1>About Page</h1>
-      </StyledCard>
+
+      <AboutCard>
+        <Button style="primary" text="Go Back" handleClick={navToHome} />
+      </AboutCard>
+
       <Footer />
     </ScreenWrapper>
   )
