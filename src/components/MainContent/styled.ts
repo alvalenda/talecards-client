f5b83f32fba06071c637dc13ba78d782e96c592f
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-14 21:01:48 -0300

    style(maincontent): create sketch style

---
+++ src/components/MainContent/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MainContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+export const MainContentTitle = styled.h1`
+  font-size: 3rem;
+  margin-top: 50px;
+`
