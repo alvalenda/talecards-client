dc3c9d8a581edc77a655c5bc37cefe7596c58408
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-14 21:01:07 -0300

    feat(app): test new home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { MainContent } from './components/MainContent'
+import { OptionsBar } from './components/OptionsBar'
 import { ThemeProviderReducer } from './components/shared/ThemeProviderReducer'
+import { Sidebar } from './components/SideBar'
 import { TalespireProvider } from './contexts/talespire/TalespireContext'
 import { GlobalStyle } from './globalStyles'
 import { AboutPage } from './pages/AboutPage'
 import { LoginPage } from './pages/LoginPage'
+import styled from 'styled-components'
 
 function App() {
   return (
@@ -11,11 +15,27 @@ function App() {
       <ThemeProviderReducer>
         <GlobalStyle />
         <BrowserRouter>
-          <Routes>
+          {/* <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<LoginPage />} />
-          </Routes>
+            <Route path="*" element={<LoginPage />} /> */}
+
+          <AppContainer className="app-container">
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<MainContent section="home" />} />
+              <Route
+                path="/section1"
+                element={<MainContent section="section1" />}
+              />
+              <Route
+                path="/section2"
+                element={<MainContent section="section2" />}
+              />
+            </Routes>
+            <OptionsBar />
+          </AppContainer>
+          {/* </Routes> */}
         </BrowserRouter>
       </ThemeProviderReducer>
     </TalespireProvider>
@@ -23,3 +43,7 @@ function App() {
 }
 
 export default App
+
+const AppContainer = styled.div`
+  display: flex;
+`
