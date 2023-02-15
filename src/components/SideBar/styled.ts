d8bd59c5e31e7c078d10891b35f70f8bd6b0e985
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-15 17:58:25 -0300

    style(sidebar): implement `expanded` as string

--- src/components/SideBar/styled.ts
+++ src/components/SideBar/styled.ts
@@ -1,11 +1,11 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, NavLinkProps } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
 export const SidebarContainer = styled.nav<SidebarContainerProps>`
-  background-color: #f5f5f5;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.backgroundVar};
   height: 100vh;
-  width: ${({ expanded }: { expanded: boolean }) =>
-    expanded ? '200px' : '50px'};
+  width: ${({ expanded }) => (expanded === 'true' ? '200px' : '50px')};
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -13,46 +13,80 @@ export const SidebarContainer = styled.nav<SidebarContainerProps>`
   transition: width 0.3s ease-in-out;
   position: relative;
 `
-export const SidebarLink = styled(NavLink)`
-  color: #333;
-  font-weight: bold;
+export const SidebarLink = styled(NavLink)<ExpandSidebarLinkProps>`
+  position: relative;
+  color: ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  font-weight: 600;
   text-decoration: none;
+  width: 90%;
   margin: 20px;
+  padding: 10px 20px;
+  text-justify: center;
+  text-indent: 2rem;
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0.65rem;
+
+    transform: translate(0, -50%);
+  }
+
   &.active {
-    color: #ff5733;
+    color: ${({ theme }) => theme.colors.interactive};
+    background-color: ${({ theme }) => theme.colors.background} !important;
   }
 `
 export const ExpandButton = styled.button<ExpandButtonProps>`
   position: absolute;
   bottom: 5rem;
-  left: ${({ expanded }) => (expanded ? '8rem' : '0.01rem')};
-  border: ${({ expanded }) => (expanded ? 'none' : '1px solid #333')};
+  left: ${({ expanded }) => (expanded === 'true' ? '8rem' : '0.01rem')};
+  border: ${({ expanded }) =>
+    expanded === 'true' ? 'none' : '1px solid #333'};
   border-radius: 33%;
 
-  color: #333;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.background};
   font-size: var(--font-size-lg);
   width: 50px;
   height: 50px;
-  background-color: #f5f5f5;
 
   transition: left 0.3s ease-in-out;
 
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    transition: transform 0.3s ease-in-out;
+
+    transform: translate(-50%, -50%)
+      ${({ expanded }) =>
+        expanded === 'true' ? 'rotate(270deg)' : 'rotate(90deg)'};
+  }
+
   ${({ expanded }) =>
-    !expanded &&
+    expanded !== 'true' &&
     css`
       right: 0;
     `}
 
   &:hover {
     cursor: pointer;
-    background: #ff5733;
+    color: var(--color-white);
+    background: ${({ theme }) => theme.colors.interactive};
   }
 `
 
 interface ExpandButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  expanded: boolean
+  expanded: string
+}
+
+interface ExpandSidebarLinkProps extends React.HTMLProps<NavLinkProps> {
+  expanded: string
 }
 
 interface SidebarContainerProps extends React.HTMLProps<HTMLDivElement> {
-  expanded: boolean
+  expanded: string
 }
