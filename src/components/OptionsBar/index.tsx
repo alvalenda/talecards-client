2d03bf88d5f88d13fdccfb200a47686bee88a2a3
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-14 21:02:16 -0300

    feat(Navbar): create `OptionsBar`

---
+++ src/components/OptionsBar/index.tsx
@@ -0,0 +1,11 @@
+import { OptionsBarContainer, OptionButton } from './styled'
+
+export function OptionsBar() {
+  return (
+    <OptionsBarContainer>
+      <OptionButton>Option 1</OptionButton>
+      <OptionButton>Option 2</OptionButton>
+      <OptionButton>Option 3</OptionButton>
+    </OptionsBarContainer>
+  )
+}
