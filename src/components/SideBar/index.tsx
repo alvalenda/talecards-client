457249dc0c0db7e3c32460283a2501e38e282e4a
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-15 17:57:45 -0300

    feat(sidebar): implement `expand` as string and deal with `SiderbarLink` content

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,22 +1,38 @@
 import { useState } from 'react'
+import { MdOutlineExpandLess } from 'react-icons/md'
+import { HiHome } from 'react-icons/hi'
+import { SiHomebrew } from 'react-icons/si'
 import { ExpandButton, SidebarContainer, SidebarLink } from './styled'
 
 export function Sidebar() {
-  const [expanded, setExpanded] = useState(true)
+  const [expanded, setExpanded] = useState<boolean>(true)
 
   const handleExpand = () => {
     setExpanded(!expanded)
   }
 
+  const getExpanded = () => {
+    return expanded.toString()
+  }
+
   return (
-    <SidebarContainer expanded={expanded}>
-      <SidebarLink to="/" tabIndex={0}>
-        Home
+    <SidebarContainer expanded={getExpanded()}>
+      <SidebarLink to="/" expanded={getExpanded()} tabIndex={0}>
+        <HiHome size={25} />
+        {getExpanded() === `true` ? 'Home' : <>&nbsp;</>}
       </SidebarLink>
-      <SidebarLink to="/section1">Section 1</SidebarLink>
-      <SidebarLink to="/section2">Section 2</SidebarLink>
-      <ExpandButton onClick={handleExpand} expanded={expanded}>
-        {expanded ? '<' : '>'}
+
+      <SidebarLink to="/section1" expanded={getExpanded()}>
+        Section 1
+      </SidebarLink>
+
+      <SidebarLink to="/section2" expanded={getExpanded()}>
+        <SiHomebrew size={25} />
+        Section 2
+      </SidebarLink>
+
+      <ExpandButton onClick={handleExpand} expanded={getExpanded()}>
+        <MdOutlineExpandLess size={30} />
       </ExpandButton>
     </SidebarContainer>
   )
