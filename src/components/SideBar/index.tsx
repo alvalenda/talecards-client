6128c084ecb326f3f1252fca6b795ab3720bcd39
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-15 23:51:33 -0300

    feat(sidebar): add `NavlinkContainer` & `UserMenu`

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -3,7 +3,13 @@ import { MdOutlineExpandLess } from 'react-icons/md'
 import { HiHome } from 'react-icons/hi'
 import { SiHomebrew } from 'react-icons/si'
 import { GiBestialFangs, GiSwordman } from 'react-icons/gi'
-import { ExpandButton, SidebarContainer, SidebarLink } from './styled'
+import {
+  ExpandButton,
+  SidebarContainer,
+  SidebarLink,
+  SidebarLinkContainer,
+} from './styled'
+import { UserMenu } from './UserMenu'
 
 export function Sidebar() {
   const [expanded, setExpanded] = useState<boolean>(true)
@@ -18,25 +24,29 @@ export function Sidebar() {
 
   return (
     <SidebarContainer expanded={getExpanded()}>
-      <SidebarLink to="/" expanded={getExpanded()} tabIndex={0}>
-        <HiHome size={25} />
-        {getExpanded() === `true` ? 'Home' : <>&nbsp;</>}
-      </SidebarLink>
+      <SidebarLinkContainer expanded={getExpanded()}>
+        <SidebarLink to="/" expanded={getExpanded()} tabIndex={0}>
+          <HiHome size={25} />
+          {getExpanded() === `true` ? 'Home' : <>&nbsp;</>}
+        </SidebarLink>
 
-      <SidebarLink to="/section1" expanded={getExpanded()}>
-        <GiSwordman size={25} />
-        {getExpanded() === `true` ? 'Personagem' : <>&nbsp;</>}
-      </SidebarLink>
+        <SidebarLink to="/section1" expanded={getExpanded()}>
+          <GiSwordman size={25} />
+          {getExpanded() === `true` ? 'Personagem' : <>&nbsp;</>}
+        </SidebarLink>
 
-      <SidebarLink to="/section2" expanded={getExpanded()}>
-        <GiBestialFangs size={25} />
-        {getExpanded() === `true` ? 'Bestiário' : <>&nbsp;</>}
-      </SidebarLink>
+        <SidebarLink to="/section2" expanded={getExpanded()}>
+          <GiBestialFangs size={25} />
+          {getExpanded() === `true` ? 'Bestiário' : <>&nbsp;</>}
+        </SidebarLink>
 
-      <SidebarLink to="/section3" expanded={getExpanded()}>
-        <SiHomebrew size={25} />
-        {getExpanded() === `true` ? 'Homebrew' : <>&nbsp;</>}
-      </SidebarLink>
+        <SidebarLink to="/section3" expanded={getExpanded()}>
+          <SiHomebrew size={25} />
+          {getExpanded() === `true` ? 'Homebrew' : <>&nbsp;</>}
+        </SidebarLink>
+      </SidebarLinkContainer>
+
+      <UserMenu expanded={getExpanded} />
 
       <ExpandButton onClick={handleExpand} expanded={getExpanded()}>
         <MdOutlineExpandLess size={30} />
