dff0ba1a9760c2b4c22cc1029227741015240e3b
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-15 23:52:31 -0300

    style(sidebar): `UserMenu` & `NavlinkContainer` styles

--- src/components/SideBar/styled.ts
+++ src/components/SideBar/styled.ts
@@ -2,16 +2,17 @@ import { NavLink, NavLinkProps } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
 export const SidebarContainer = styled.nav<SidebarContainerProps>`
+  position: relative;
   color: ${({ theme }) => theme.colors.primary};
   background-color: ${({ theme }) => theme.colors.backgroundVar};
-  height: 100vh;
   width: ${({ expanded }) => (expanded === 'true' ? '200px' : '50px')};
+  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   transition: width 0.3s ease-in-out;
-  position: relative;
+  overflow: hidden;
   z-index: 1;
 `
 export const SidebarLink = styled(NavLink)<ExpandSidebarLinkProps>`
@@ -42,12 +43,11 @@ export const SidebarLink = styled(NavLink)<ExpandSidebarLinkProps>`
 `
 export const ExpandButton = styled.button<ExpandButtonProps>`
   position: absolute;
-  bottom: 5rem;
-  left: ${({ expanded }) => (expanded === 'true' ? '8rem' : '0.01rem')};
+  bottom: 11.5rem;
+  left: ${({ expanded }) => (expanded === 'true' ? '8.2rem' : '0.01rem')};
   border: ${({ expanded }) =>
     expanded === 'true' ? 'none' : '1px solid #333'};
   border-radius: 33%;
-
   color: ${({ theme }) => theme.colors.primary};
   background-color: ${({ theme }) => theme.colors.background};
   font-size: var(--font-size-lg);
@@ -81,6 +81,68 @@ export const ExpandButton = styled.button<ExpandButtonProps>`
   }
 `
 
+export const SidebarLinkContainer = styled.div<SidebarContainerProps>`
+  position: absolute;
+  top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${({ expanded }) => (expanded === 'true' ? '200px' : '50px')};
+  transition: width 0.3s ease-in-out;
+`
+
+export const UserMenuButton = styled.button<UserMenuButtonProps>`
+  position: relative;
+  background: none;
+  border: none;
+  outline: inherit;
+  text-decoration: none;
+  color: inherit;
+  font: inherit;
+  border-radius: 5px;
+  font-weight: 600;
+  width: ${({ expanded }) => (expanded === 'true' ? '95%' : '50px')};
+  margin: 5px;
+  padding: 10px 20px;
+  text-indent: 2rem;
+  text-align: left;
+  overflow: hidden;
+  cursor: pointer;
+
+  transition: all 0.3s ease-in-out;
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0.65rem;
+    transform: translate(0, -50%);
+  }
+
+  &.active {
+    color: ${({ theme }) => theme.colors.interactive};
+    background-color: ${({ theme }) => theme.colors.background} !important;
+  }
+`
+
+export const UserMenuContainer = styled.div<SidebarContainerProps>`
+  position: absolute;
+  top: calc(100% - 12rem);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: 10px;
+  margin-top: 10px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+  border-top: 1px solid ${({ theme }) => theme.colors.primary};
+  width: ${({ expanded }) => (expanded === 'true' ? '200px' : '50px')};
+  z-index: 1;
+
+  transition: width 0.3s ease-in-out;
+`
+
 interface ExpandButtonProps extends React.HTMLProps<HTMLButtonElement> {
   expanded: string
 }
@@ -90,5 +152,9 @@ interface ExpandSidebarLinkProps extends React.HTMLProps<NavLinkProps> {
 }
 
 interface SidebarContainerProps extends React.HTMLProps<HTMLDivElement> {
+  expanded: string
+}
+
+interface UserMenuButtonProps extends React.HTMLProps<HTMLButtonElement> {
   expanded: string
 }
