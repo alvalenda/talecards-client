3e7698d56feae30d22f2c40278b1a84260856aed
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-16 10:14:29 -0300

    feat(sidebar): ExpandButton out of container

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -23,34 +23,36 @@ export function Sidebar() {
   }
 
   return (
-    <SidebarContainer expanded={getExpanded()}>
-      <SidebarLinkContainer expanded={getExpanded()}>
-        <SidebarLink to="/" expanded={getExpanded()} tabIndex={0}>
-          <HiHome size={25} />
-          {getExpanded() === `true` ? 'Home' : <>&nbsp;</>}
-        </SidebarLink>
-
-        <SidebarLink to="/section1" expanded={getExpanded()}>
-          <GiSwordman size={25} />
-          {getExpanded() === `true` ? 'Personagem' : <>&nbsp;</>}
-        </SidebarLink>
-
-        <SidebarLink to="/section2" expanded={getExpanded()}>
-          <GiBestialFangs size={25} />
-          {getExpanded() === `true` ? 'Bestiário' : <>&nbsp;</>}
-        </SidebarLink>
-
-        <SidebarLink to="/section3" expanded={getExpanded()}>
-          <SiHomebrew size={25} />
-          {getExpanded() === `true` ? 'Homebrew' : <>&nbsp;</>}
-        </SidebarLink>
-      </SidebarLinkContainer>
-
-      <UserMenu expanded={getExpanded} />
-
+    <>
       <ExpandButton onClick={handleExpand} expanded={getExpanded()}>
         <MdOutlineExpandLess size={30} />
       </ExpandButton>
-    </SidebarContainer>
+
+      <SidebarContainer expanded={getExpanded()}>
+        <SidebarLinkContainer expanded={getExpanded()}>
+          <SidebarLink to="/" expanded={getExpanded()} tabIndex={0}>
+            <HiHome size={25} />
+            {getExpanded() === `true` ? 'Home' : <>&nbsp;</>}
+          </SidebarLink>
+
+          <SidebarLink to="/section1" expanded={getExpanded()}>
+            <GiSwordman size={25} />
+            {getExpanded() === `true` ? 'Personagem' : <>&nbsp;</>}
+          </SidebarLink>
+
+          <SidebarLink to="/section2" expanded={getExpanded()}>
+            <GiBestialFangs size={25} />
+            {getExpanded() === `true` ? 'Bestiário' : <>&nbsp;</>}
+          </SidebarLink>
+
+          <SidebarLink to="/section3" expanded={getExpanded()}>
+            <SiHomebrew size={25} />
+            {getExpanded() === `true` ? 'Homebrew' : <>&nbsp;</>}
+          </SidebarLink>
+        </SidebarLinkContainer>
+
+        <UserMenu expanded={getExpanded} />
+      </SidebarContainer>
+    </>
   )
 }
