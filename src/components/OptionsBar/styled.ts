f56b092900eacc4f77bdfaf1d506c711dab460bc
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-16 13:55:12 -0300

    style(optionsbar): box-shadow replaces margin

--- src/components/OptionsBar/styled.ts
+++ src/components/OptionsBar/styled.ts
@@ -7,6 +7,8 @@ export const OptionsBarContainer = styled.div`
   justify-content: center;
   padding-left: 50px;
   width: 100%;
+  box-shadow: 0 0 5px 0 rgba(255, 255, 255, 0.2), 0 0 5px 0 rgba(0, 0, 0, 0.2);
+
   background-color: ${({ theme }) => theme.colors.backgroundVar};
 `
 
