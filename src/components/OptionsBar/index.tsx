ae857f8136837c1ba67d03392933d2722feab6bd
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-17 19:47:16 -0300

    feat(optionsBar): add `handRoll`

--- src/components/OptionsBar/index.tsx
+++ src/components/OptionsBar/index.tsx
@@ -1,11 +1,24 @@
-import { OptionsBarContainer, OptionButton } from './styled'
+import { RollTestPayload } from '@/common/types/dice-roll'
+import { diceHundredRoll, diceTestHelper } from '@/common/utils/dice.helper'
+import { OptionButton, OptionsBarContainer } from './styled'
 
 export function OptionsBar() {
+  function handleRoll(target: string) {
+    const roll: Required<RollTestPayload> = {
+      ...diceHundredRoll(),
+      ...{ target: parseInt(target) || 0 },
+    }
+
+    const rollTest = diceTestHelper(roll)
+
+    console.table(rollTest)
+  }
+
   return (
     <OptionsBarContainer>
       <OptionButton>Option 1</OptionButton>
       <OptionButton>Option 2</OptionButton>
-      <OptionButton>Option 3</OptionButton>
+      <OptionButton onClick={() => handleRoll('50')}>Rolar d100</OptionButton>
     </OptionsBarContainer>
   )
 }
