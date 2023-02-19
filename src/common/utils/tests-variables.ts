837e5111d72eddfe21c4570d911c29676b987532
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-19 10:30:12 -0300

    feat(tests-variables): create BodyPartsTable

--- src/common/utils/tests-variables.ts
+++ src/common/utils/tests-variables.ts
@@ -1,13 +1,13 @@
 import { Modifier } from '../types/dice-roll'
 
 export const testDifficult: TestDifficult[] = [
-  { difficult: 'very_easy', modifier: 60 },
-  { difficult: 'easy', modifier: 40 },
-  { difficult: 'average', modifier: 20 },
-  { difficult: 'challenge', modifier: 0 },
-  { difficult: 'difficult', modifier: -20 },
-  { difficult: 'hard', modifier: -40 },
-  { difficult: 'very_hard', modifier: -60 },
+  { difficult: 'Muito Fácil', modifier: 60 },
+  { difficult: 'Fácil', modifier: 40 },
+  { difficult: 'Mediana', modifier: 20 },
+  { difficult: 'Desafiadora', modifier: 0 },
+  { difficult: 'Complicada', modifier: -10 },
+  { difficult: 'Difícil', modifier: -20 },
+  { difficult: 'Muito Difícil', modifier: -30 },
 ]
 
 export const testSuccessOutcomes: TestOutcome[] = [
@@ -30,9 +30,32 @@ export const testFailureOutcomes: TestOutcome[] = [
   { sl: -6, outcome: 'Falha Catastrófica' },
 ]
 
+// como validar resultRange?
+
+export const bodyPartsTable: BodyPartsOption[] = [
+  { bodyPart: 'Cabeça', resultRange: [1, 9] },
+  {
+    bodyPart: 'Braço Primário',
+    resultRange: [10, 24],
+  },
+  {
+    bodyPart: 'Braço Secundário',
+    resultRange: [25, 44],
+  },
+  { bodyPart: 'Tronco', resultRange: [45, 79] },
+  {
+    bodyPart: 'Perna Primária',
+    resultRange: [80, 89],
+  },
+  {
+    bodyPart: 'Perna Secundária',
+    resultRange: [90, 100],
+  },
+]
+
 export const testDifficultOptions = testDifficult.map((difficult) => ({
-  value: difficult.difficult,
-  label: difficult.difficult,
+  difficult: difficult.difficult,
+  modifier: difficult.modifier,
 }))
 
 export type TestOutcome = {
@@ -46,10 +69,31 @@ export type TestDifficult = {
 }
 
 export type Difficulties =
-  | 'very_easy'
-  | 'easy'
-  | 'average'
-  | 'challenge'
-  | 'difficult'
-  | 'hard'
-  | 'very_hard'
+  | 'Muito Fácil'
+  | 'Fácil'
+  | 'Mediana'
+  | 'Desafiadora'
+  | 'Complicada'
+  | 'Difícil'
+  | 'Muito Difícil'
+
+export type BodyPartsOption = {
+  bodyPart: BodyParts
+  resultRange: [number, number]
+}
+export type BodyParts =
+  | 'Cabeça'
+  | 'Braço'
+  | 'Braço Primário'
+  | 'Braço Secundário'
+  | 'Tronco'
+  | 'Perna'
+  | 'Perna Primária'
+  | 'Perna Secundária'
+
+type ValidNumber = number & { __brand: 'ValidNumber' }
+
+function validNumber(n: number): ValidNumber {
+  if (n < 1 || n > 100) throw new Error('número inválido')
+  return n as ValidNumber
+}
