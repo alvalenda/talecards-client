5ebffe4c172a28630727275649ea0d42024ce555
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-19 19:51:18 -0300

    style(MainContent): `MainContentContainer` & `MainContentTitleContainer`

--- src/components/MainContent/styled.ts
+++ src/components/MainContent/styled.ts
@@ -1,16 +1,38 @@
 import styled from 'styled-components'
 
 export const MainContentContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colors.background};
   width: 100%;
+  height: 100vh;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.background};
+  overflow: hidden;
+  z-index: 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export const MainContentTitleContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 8rem;
+  margin-top: 10px;
 `
 
 export const MainContentTitle = styled.h1`
-  font-size: 3rem;
-  margin-top: 50px;
+  font-size: var(--font-size-xxxl);
+  text-align: center;
+  margin-top: 10px;
   color: ${({ theme }) => theme.colors.primary};
 `
