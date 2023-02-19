e89e1af6fa2ed923b3d6b4afd5673b3a95bc7892
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-19 14:00:30 -0300

    feat(monstersheet): add `MonsterSheet` component

---
+++ src/components/shared/MonsterSheet/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export function MonsterSheet({}: Props) {
+  // component for MonsterSheet in WFRP 4e system
+  return (
+    <>
+      <div>name</div>
+      <div>description</div>
+      <div>type</div>
+      <div>size</div>
+      <div>movement</div>
+      <div>weaponSkill</div>
+      <div>ballisticSkill</div>
+      <div>strength</div>
+      <div>toughness</div>
+      <div>agility</div>
+      <div>intelligence</div>
+      <div>willpower</div>
+      <div>fellowship</div>
+      <div>attacks</div>
+      <div>wounds</div>
+      <div>magic</div>
+      <div>traits</div>
+      <div>skills</div>
+      <div>talents</div>
+      <div>armour</div>
+      <div>weapons</div>
+    </>
+  )
+}
+
+type Props = {}
