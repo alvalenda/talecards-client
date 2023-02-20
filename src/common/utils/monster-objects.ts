75a1bf1d2d383339057338461056ea81f78c5d7d
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-20 19:50:03 -0300

    feat(util): create `monster-objects`

---
+++ src/common/utils/monster-objects.ts
@@ -0,0 +1,12 @@
+import { Monster } from '../types/character'
+import { characteristicsSample } from './attributes-objects'
+
+export const MonsterSampleSheet: Monster = {
+  id: '0',
+  name: 'Monster Sample',
+  description: 'Um monstro que vive na floresta',
+  attributes: characteristicsSample,
+  traits: [],
+  skills: [],
+  talents: [],
+}
