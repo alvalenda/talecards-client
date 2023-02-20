e279e563b915052ad003d198d3ce8d3f433df7d4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-20 19:50:17 -0300

    feat(util): create `player-objects`

---
+++ src/common/utils/player-objects.ts
@@ -0,0 +1,42 @@
+import { Player } from '../types/character'
+import { attributesBlank } from './attributes-objects'
+
+export const playerBlank: Player = {
+  id: '0',
+  name: '',
+  description: '',
+  attributes: attributesBlank,
+  avatar: '',
+  skills: [],
+  talents: [],
+  weapons: [],
+  armors: [],
+  trappings: [],
+  inventory: [],
+  notes: '',
+  history: '',
+}
+
+export const playerSample: Player = {
+  id: '0',
+  name: 'Player Sample',
+  description: 'Um personagem que vive na floresta',
+  attributes: attributesBlank,
+  avatar: '',
+  skills: [
+    {
+      name: 'Animal Care',
+      name_br: 'Lidar com Animais',
+      keyAtribute: 'Intelligence',
+      advances: 0,
+      specialization: '',
+    },
+  ],
+  talents: [],
+  weapons: [],
+  armors: [],
+  trappings: [],
+  inventory: [],
+  notes: '',
+  history: '',
+}
