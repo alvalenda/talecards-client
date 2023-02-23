d0082422e6532044b14342f192082fc806c0c1be
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-23 11:56:39 -0300

    type(attributes): create file

---
+++ src/common/types/attributes.d.ts
@@ -0,0 +1,71 @@
+export type Attribute = {
+  name: AttributeName | CharacteristicName
+  name_br: AttributeBR | CharacteristicBR
+  baseValue: number
+  advances: number
+  short: AttributeShort | CharacteristicShort
+}
+
+export type Characteristic = {
+  name: CharacteristicName
+  name_br: CharacteristicBR
+  baseValue: number
+  advances: number
+  short: CharacteristicShort | 'M' | 'W'
+}
+
+export type CharacteristicName =
+  | 'Weapon Skill'
+  | 'Ballistic Skill'
+  | 'Strength'
+  | 'Toughness'
+  | 'Initiative'
+  | 'Agility'
+  | 'Dexterity'
+  | 'Intelligence'
+  | 'Will Power'
+  | 'Fellowship'
+  | 'Movement'
+  | 'Wounds'
+
+export type CharacteristicBR =
+  | 'Combate Armado'
+  | 'Balística'
+  | 'Força'
+  | 'Robustez'
+  | 'Iniciativa'
+  | 'Agilidade'
+  | 'Destreza'
+  | 'Inteligência'
+  | 'Vontade'
+  | 'Carisma'
+  | 'Movimento'
+  | 'Ferimentos'
+
+export type AttributeName =
+  | 'Fate'
+  | 'Fortune'
+  | 'Resilience'
+  | 'Resolve'
+  | 'Motivation'
+
+export type AttributeBR =
+  | 'Destino'
+  | 'Sorte'
+  | 'Resiliência'
+  | 'Determinação'
+  | 'Motivação'
+
+export type CharacteristicShort =
+  | 'WS'
+  | 'BS'
+  | 'S'
+  | 'T'
+  | 'I'
+  | 'Ag'
+  | 'Dex'
+  | 'Int'
+  | 'WP'
+  | 'Fel'
+
+export type AttributeShort = 'W' | 'Fate' | 'For' | 'Res' | 'Det' | 'Mot' | 'M'
