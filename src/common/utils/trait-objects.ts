c0b9e4e1b9caee299ac70f468a0fe92d7b894664
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-23 21:36:48 -0300

    feat(utils): create `trait-object`

---
+++ src/common/utils/trait-objects.ts
@@ -0,0 +1,84 @@
+export const traits = []
+
+/* Traits
+Afraid (Target)
+Amphibious
+Arboreal
+Animosity (Target)
+Armour (Rating)(Including TB)
+Belligerent
+Bestial
+Big
+Bite (Rating)
+Blessed (Various)
+Bounce
+Breath (Rating)(Type)
+Brute
+Champion
+Chill Grasp
+Clever
+Cold-blooded
+Constrictor
+Construct
+Corrosive Blood
+Corruption
+Cunning
+Dark Vision
+Daemonic (Target)
+Die Hard
+Disease (Type)
+Distracting
+Elite
+Ethereal
+Fast
+Fear (Rating)
+Flight (Rating)
+Frenzy
+Fury
+Ghostly Howl
+Grim (Rating)
+Hardy
+Hatred (Target)
+Horns (Rating)(Feature)
+Hungry
+Immunity (Type)
+Immunity to
+Psychology
+Infected
+Infestation
+Leader
+Magical
+Miracles (various)
+Night Vision
+Painless
+Petrifying Gaze
+Prejudice (Target)
+Ranged (Rating)
+Rear
+Regenerate
+Skittish
+Spellcaster
+Stealthy
+Size
+Stride
+Stupid
+Swamp-strider
+Swarm
+Tail Attack (Rating)
+# Tentacles (Rating)
+Territorial
+Terror (Rating)
+Trained
+Tongue Attack (Rating)(Range)
+Tough
+Tracker
+Undead
+Unstable
+Vampiric
+Venom (Difficulty)
+Vomit
+Ward (Rating)
+Wall crawler
+Weapon (Rating)
+Web (Rating)
+                        */
